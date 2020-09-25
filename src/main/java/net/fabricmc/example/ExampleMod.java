37e88f74c07f4eb05835fb9105f6c44e3704611b
SD <harrypotter.starwars.shrish@gmail.com>
2020-09-25 20:16:33 +0530

    Recipe
     Changes to be committed:
    	modified:   src/main/java/net/fabricmc/example/ExampleMod.java
    	new file:   src/main/java/net/fabricmc/example/recipe/ExampleRecipe.java
    	new file:   src/main/java/net/fabricmc/example/recipe/ExampleRecipeSerializer.java

--- src/main/java/net/fabricmc/example/ExampleMod.java
+++ src/main/java/net/fabricmc/example/ExampleMod.java
@@ -1,8 +1,13 @@
 package net.fabricmc.example;
 
+import net.minecraft.recipe.RecipeType;
+
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.example.recipe.ExampleRecipe;
 
 public class ExampleMod implements ModInitializer {
+    public static final RecipeType<ExampleRecipe> RECIPE_TYPE = RecipeType.register("example:recipe_type");
+
     @Override
     public void onInitialize() {
 
